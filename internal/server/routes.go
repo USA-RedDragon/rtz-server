2ee3d26faad8475e6f2156b154bd870a811bebae
Jacob McSwain <jacob@mcswain.dev>
2024-06-27 13:45:30 -0500

    pilotauth: bugfixes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -56,7 +56,7 @@ func applyRoutes(r *gin.Engine, config *config.Config, eventsChannel chan events
 	apiV2 := r.Group("/v2")
 	apiV2.POST("/pilotauth", func(c *gin.Context) {
 		if !config.Registration.Enabled {
-			c.JSON(http.StatusForbidden, gin.H{"error": "Registration is disabled"})
+			c.JSON(http.StatusNotFound, gin.H{"error": "Registration is disabled"})
 			return
 		}
 		param_imei, ok := c.GetQuery("imei")
@@ -199,7 +199,7 @@ func applyRoutes(r *gin.Engine, config *config.Config, eventsChannel chan events
 
 		slog.Info("Pilot Auth", "imei", imei, "imei2", imei2, "serial", param_serial, "claims", claims, "dongle_id", dongleID)
 
-		c.JSON(http.StatusOK, gin.H{})
+		c.JSON(http.StatusOK, gin.H{"dongle_id": dongleID})
 	})
 
 	r.NoRoute(func(c *gin.Context) {
