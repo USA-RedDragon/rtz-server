996daa5e4e75de0e5e2f20a322eee6440ed85d07
Jacob McSwain <jacob@mcswain.dev>
2024-06-27 14:26:31 -0500

    middleware: implement jwt cookie auth

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -1,11 +1,17 @@
 package server
 
 import (
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
 
 	"github.com/USA-RedDragon/connect-server/internal/config"
+	"github.com/USA-RedDragon/connect-server/internal/db/models"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
@@ -63,6 +69,65 @@ func requireAuth(_ *config.Config) gin.HandlerFunc {
 // Requires a jwt cookie
 func requireCookieAuth(_ *config.Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		cookie, err := c.Cookie("jwt")
+		if err != nil || cookie == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
+		}
+
+		dongleID, ok := c.Params.Get("dongle_id")
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "dongle_id is required"})
+			return
+		}
+
+		db, ok := c.MustGet("db").(*gorm.DB)
+		if !ok {
+			slog.Error("Failed to get db from context")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Try again later"})
+			return
+		}
+
+		// Verify the token
+		token, err := jwt.NewParser(jwt.WithValidMethods([]string{jwt.SigningMethodRS256.Name})).Parse(cookie, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+				return nil, fmt.Errorf("invalid signing method: %s", token.Header["alg"])
+			}
+			claims := token.Claims.(*jwt.RegisteredClaims)
+
+			// ParseWithClaims will skip expiration check
+			// if expiration has default value;
+			// forcing a check and exiting if not set
+			if claims.ExpiresAt == nil {
+				return nil, errors.New("token has no expiration")
+			}
+
+			device, err := models.FindDeviceByDongleID(db, dongleID)
+			if err != nil {
+				return nil, fmt.Errorf("failed to find device: %w", err)
+			}
+
+			blk, _ := pem.Decode([]byte(device.PublicKey))
+			key, err := x509.ParsePKIXPublicKey(blk.Bytes)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse public key: %w", err)
+			}
+			return key, nil
+		})
+		if err != nil {
+			slog.Error("Failed to parse token", "error", err)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
+		}
+
+		if !token.Valid {
+			slog.Error("Invalid token")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
+		}
+
+		fmt.Println(token.Claims)
+
+		c.Next()
 	}
 }
