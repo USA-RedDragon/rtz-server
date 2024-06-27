eb1f57e49596f6ae433bdb47663bd10cfb9fca78
Jacob McSwain <jacob@mcswain.dev>
2024-06-27 15:08:23 -0500

    middleware: implement JWT header auth

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"strings"
 
 	"github.com/USA-RedDragon/connect-server/internal/config"
 	"github.com/USA-RedDragon/connect-server/internal/db/models"
@@ -62,7 +63,70 @@ func dbMiddleware(db *gorm.DB) gin.HandlerFunc {
 // Requires an Authorization: JWT <token> header
 func requireAuth(_ *config.Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
+		}
+
+		if !strings.HasPrefix(authHeader, "JWT ") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
+		}
+
+		jwtString := strings.TrimPrefix(authHeader, "JWT ")
+
+		device, ok := c.MustGet("device").(*models.Device)
+		if !ok {
+			slog.Error("Failed to get device from context")
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		type Claims struct {
+			jwt.RegisteredClaims
+			Identity string `json:"identity"`
+		}
+		claims := new(Claims)
+
+		// Verify the token
+		token, err := jwt.NewParser(jwt.WithValidMethods([]string{jwt.SigningMethodRS256.Name})).ParseWithClaims(jwtString, claims, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+				return nil, fmt.Errorf("invalid signing method: %s", token.Header["alg"])
+			}
+			claims = token.Claims.(*Claims)
+
+			// ParseWithClaims will skip expiration check
+			// if expiration has default value;
+			// forcing a check and exiting if not set
+			if claims.ExpiresAt == nil {
+				return nil, errors.New("token has no expiration")
+			}
+
+			if claims.Identity != device.DongleID {
+				return nil, errors.New("identity does not match device")
+			}
+
+			blk, _ := pem.Decode([]byte(device.PublicKey))
+			key, err := x509.ParsePKIXPublicKey(blk.Bytes)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse public key: %w", err)
+			}
+			return key, nil
+		})
+		if err != nil {
+			slog.Error("Failed to parse token", "error", err)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
+		}
+
+		if !token.Valid {
+			slog.Error("Invalid token")
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
+		}
+
+		c.Next()
 	}
 }
 
