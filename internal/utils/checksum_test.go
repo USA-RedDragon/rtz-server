b585f887beca9c68e2d0fad6174928027cdc69e2
Jacob McSwain <jacob@mcswain.dev>
2024-07-10 22:59:32 -0500

    write tests for luhn checksums

---
+++ internal/utils/checksum_test.go
@@ -0,0 +1,42 @@
+package utils_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/USA-RedDragon/rtz-server/internal/utils"
+)
+
+func TestLuhn(t *testing.T) {
+	t.Parallel()
+
+	validLuhns := []int{
+		0,
+		7082049359,
+		5753425163,
+		711087701572692,
+	}
+
+	invalidLuhns := []int{
+		1,
+		7082049358,
+		5753425164,
+		711087701572693,
+	}
+
+	for _, tt := range validLuhns {
+		t.Run(fmt.Sprintf("LuhnValid[%d]", tt), func(t *testing.T) {
+			if got := utils.LuhnValid(tt); !got {
+				t.Errorf("LuhnValid() = %v, want %v", got, true)
+			}
+		})
+	}
+
+	for _, tt := range invalidLuhns {
+		t.Run(fmt.Sprintf("LuhnInValid[%d]", tt), func(t *testing.T) {
+			if got := utils.LuhnValid(tt); got {
+				t.Errorf("LuhnInvalidValid() = %v, want %v", got, false)
+			}
+		})
+	}
+}
