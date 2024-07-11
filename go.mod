80bb604625064d23b021cecfadce89979cbca7e7
Jacob McSwain <jacob@mcswain.dev>
2024-07-11 00:48:30 -0500

    db: support mysql and postgres

--- go.mod
+++ go.mod
@@ -24,6 +24,8 @@ require (
 	go.opentelemetry.io/otel/trace v1.28.0
 	golang.org/x/sync v0.7.0
 	gopkg.in/yaml.v3 v3.0.1
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/driver/postgres v1.5.9
 	gorm.io/gorm v1.25.10
 )
 
@@ -44,8 +46,13 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
--- internal/db/db.go
+++ internal/db/db.go
@@ -5,15 +5,74 @@ import (
 	"runtime"
 	"time"
 
-	"github.com/USA-RedDragon/rtz-server/internal/config"
+	configPkg "github.com/USA-RedDragon/rtz-server/internal/config"
 	"github.com/USA-RedDragon/rtz-server/internal/db/models"
 	"github.com/glebarez/sqlite"
 	"github.com/uptrace/opentelemetry-go-extra/otelgorm"
 	"gorm.io/gorm"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 )
 
-func MakeDB(config *config.Config) (db *gorm.DB, err error) {
-	db, err = gorm.Open(sqlite.Open(config.Persistence.Database+"?_pragma=foreign_keys(1)&_pragma=journal_mode(WAL)"), &gorm.Config{})
+func getDialect(config *configPkg.Config) gorm.Dialector {
+	var dialector gorm.Dialector
+	switch config.Persistence.Database.Driver {
+	case configPkg.DatabaseDriverSQLite:
+		dialector = sqlite.Open(
+			config.Persistence.Database.Database + "?_pragma=foreign_keys(1)&_pragma=journal_mode(WAL)" + "&" + config.Persistence.Database.ExtraParameters,
+		)
+	case configPkg.DatabaseDriverMySQL:
+		hasUser := config.Persistence.Database.Username != ""
+		hasPassword := config.Persistence.Database.Password != ""
+		hasUserAndPassword := hasUser && hasPassword
+		prefix := ""
+		if hasUserAndPassword {
+			prefix = fmt.Sprintf("%s:%s@", config.Persistence.Database.Username, config.Persistence.Database.Password)
+		} else if hasUser {
+			prefix = fmt.Sprintf("%s@", config.Persistence.Database.Username)
+		} else if hasPassword {
+			prefix = fmt.Sprintf(":%s@", config.Persistence.Database.Password)
+		}
+		portStr := ""
+		if config.Persistence.Database.Port != 0 {
+			portStr = fmt.Sprintf(":%d", config.Persistence.Database.Port)
+		}
+		extraParamsStr := ""
+		if config.Persistence.Database.ExtraParameters != "" {
+			extraParamsStr = "&" + config.Persistence.Database.ExtraParameters
+		}
+		dsn := fmt.Sprintf("%stcp(%s%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&%s",
+			prefix,
+			config.Persistence.Database.Host,
+			portStr,
+			config.Persistence.Database.Database,
+			extraParamsStr)
+		dialector = mysql.Open(dsn)
+	case configPkg.DatabaseDriverPostgres:
+		dsn := "host=" + config.Persistence.Database.Host + " dbname=" + config.Persistence.Database.Database
+		if config.Persistence.Database.Port != 0 {
+			dsn += fmt.Sprintf(" port=%d", config.Persistence.Database.Port)
+		}
+		if config.Persistence.Database.Username != "" {
+			dsn += " user=" + config.Persistence.Database.Username
+		}
+		if config.Persistence.Database.Password != "" {
+			dsn += " password=" + config.Persistence.Database.Password
+		}
+		if config.Persistence.Database.ExtraParameters != "" {
+			dsn += " " + config.Persistence.Database.ExtraParameters
+		}
+		dialector = postgres.New(postgres.Config{
+			DSN:                  dsn,
+			PreferSimpleProtocol: true,
+		})
+	}
+	return dialector
+}
+
+func MakeDB(config *configPkg.Config) (db *gorm.DB, err error) {
+	db, err = gorm.Open(getDialect(config))
 	if err != nil {
 		return db, fmt.Errorf("failed to open database: %w", err)
 	}
