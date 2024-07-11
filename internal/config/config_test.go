73a5d49371df3e6b2a87333d7825161c6daa06b9
Jacob McSwain <jacob@mcswain.dev>
2024-07-10 23:43:17 -0500

    config: test all the environment variables

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -125,6 +125,24 @@ func TestEnvConfig(t *testing.T) {
 	t.Setenv("HTTP__PORT", "8087")
 	t.Setenv("HTTP__METRICS__PORT", "8088")
 	t.Setenv("HTTP__METRICS__IPV4_HOST", "0.0.0.0")
+	t.Setenv("HTTP__METRICS__IPV6_HOST", "::0")
+	t.Setenv("HTTP__IPV4_HOST", "127.0.0.1")
+	t.Setenv("HTTP__IPV6_HOST", "::1")
+	t.Setenv("HTTP__PPROF__ENABLED", "true")
+	t.Setenv("HTTP__TRUSTED_PROXIES", "127.0.0.1,127.0.0.2")
+	t.Setenv("HTTP__METRICS__ENABLED", "true")
+	t.Setenv("HTTP__TRACING__ENABLED", "true")
+	t.Setenv("HTTP__TRACING__OTLP_ENDPOINT", "http://localhost:4317")
+	t.Setenv("HTTP__CORS_HOSTS", "http://localhost:8080,http://localhost:8081")
+	t.Setenv("HTTP__BACKEND_URL", "http://localhost:8081")
+	t.Setenv("PERSISTENCE__DATABASE", "test.sqlite3")
+	t.Setenv("PERSISTENCE__UPLOADS", "notuploads")
+	t.Setenv("REGISTRATION__ENABLED", "true")
+	t.Setenv("AUTH__GOOGLE__CLIENT_ID", "googleid")
+	t.Setenv("AUTH__GOOGLE__CLIENT_SECRET", "googlesecret")
+	t.Setenv("AUTH__GITHUB__CLIENT_ID", "githubid")
+	t.Setenv("AUTH__GITHUB__CLIENT_SECRET", "githubsecret")
+
 	config, err := config.LoadConfig(cmd)
 	if err != nil {
 		t.Errorf("unexpected error: %v", err)
@@ -137,5 +155,68 @@ func TestEnvConfig(t *testing.T) {
 	}
 	if config.HTTP.Metrics.IPV4Host != "0.0.0.0" {
 		t.Errorf("unexpected HTTP metrics IPv4 host: %s", config.HTTP.Metrics.IPV4Host)
+	}
+	if config.HTTP.Metrics.IPV6Host != "::0" {
+		t.Errorf("unexpected HTTP metrics IPv6 host: %s", config.HTTP.Metrics.IPV6Host)
+	}
+	if config.HTTP.IPV4Host != "127.0.0.1" {
+		t.Errorf("unexpected HTTP IPv4 host: %s", config.HTTP.IPV4Host)
+	}
+	if config.HTTP.IPV6Host != "::1" {
+		t.Errorf("unexpected HTTP IPv6 host: %s", config.HTTP.IPV6Host)
+	}
+	if !config.HTTP.PProf.Enabled {
+		t.Error("unexpected HTTP pprof enabled")
+	}
+	if len(config.HTTP.TrustedProxies) != 2 {
+		t.Errorf("unexpected HTTP trusted proxies: %v", config.HTTP.TrustedProxies)
+	}
+	if config.HTTP.TrustedProxies[0] != "127.0.0.1" {
+		t.Errorf("unexpected HTTP trusted proxy: %s", config.HTTP.TrustedProxies[0])
+	}
+	if config.HTTP.TrustedProxies[1] != "127.0.0.2" {
+		t.Errorf("unexpected HTTP trusted proxy: %s", config.HTTP.TrustedProxies[1])
+	}
+	if !config.HTTP.Metrics.Enabled {
+		t.Error("unexpected HTTP metrics enabled")
+	}
+	if !config.HTTP.Tracing.Enabled {
+		t.Error("unexpected HTTP tracing enabled")
+	}
+	if config.HTTP.Tracing.OTLPEndpoint != "http://localhost:4317" {
+		t.Errorf("unexpected HTTP tracing OTLP endpoint: %s", config.HTTP.Tracing.OTLPEndpoint)
+	}
+	if len(config.HTTP.CORSHosts) != 2 {
+		t.Errorf("unexpected HTTP CORS hosts: %v", config.HTTP.CORSHosts)
+	}
+	if config.HTTP.CORSHosts[0] != "http://localhost:8080" {
+		t.Errorf("unexpected HTTP CORS host: %s", config.HTTP.CORSHosts[0])
+	}
+	if config.HTTP.CORSHosts[1] != "http://localhost:8081" {
+		t.Errorf("unexpected HTTP CORS host: %s", config.HTTP.CORSHosts[1])
+	}
+	if config.HTTP.BackendURL != "http://localhost:8081" {
+		t.Errorf("unexpected HTTP backend URL: %s", config.HTTP.BackendURL)
+	}
+	if config.Persistence.Database != "test.sqlite3" {
+		t.Errorf("unexpected persistence database: %s", config.Persistence.Database)
+	}
+	if config.Persistence.Uploads != "notuploads" {
+		t.Errorf("unexpected persistence uploads: %s", config.Persistence.Uploads)
+	}
+	if !config.Registration.Enabled {
+		t.Error("unexpected registration enabled")
+	}
+	if config.Auth.Google.ClientID != "googleid" {
+		t.Errorf("unexpected Google client ID: %s", config.Auth.Google.ClientID)
+	}
+	if config.Auth.Google.ClientSecret != "googlesecret" {
+		t.Errorf("unexpected Google client secret: %s", config.Auth.Google.ClientSecret)
+	}
+	if config.Auth.GitHub.ClientID != "githubid" {
+		t.Errorf("unexpected GitHub client ID: %s", config.Auth.GitHub.ClientID)
+	}
+	if config.Auth.GitHub.ClientSecret != "githubsecret" {
+		t.Errorf("unexpected GitHub client secret: %s", config.Auth.GitHub.ClientSecret)
 	}
 }
