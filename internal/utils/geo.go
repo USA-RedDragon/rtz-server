fca48a6763020463a052dc828026da4ef9be4494
Jacob McSwain <jacob@mcswain.dev>
2024-07-28 13:30:18 -0500

    geo: haversine: adjust parameter names

--- internal/utils/geo.go
+++ internal/utils/geo.go
@@ -10,15 +10,13 @@ func degToRad(deg float64) float64 {
 
 // Haversine returns the distance between two GPS coordinates in meters.
 func Haversine(startLat, startLng, endLat, endLng float64) float64 {
-	endLatRads := degToRad(endLat)
-	endLngRads := degToRad(endLng)
-	startLatRads := degToRad(startLat)
-	startLngRads := degToRad(startLng)
+	phi1 := degToRad(startLat)
+	phi2 := degToRad(endLat)
+	deltaPhi := degToRad(endLat - startLat)
+	deltaLambda := degToRad(endLng - startLng)
 
-	deltaLat := math.Abs(endLatRads - startLatRads)
-	deltaLng := math.Abs(endLngRads - startLngRads)
-
-	a := math.Pow(math.Sin(deltaLat/2), 2) + math.Cos(startLatRads)*math.Cos(endLatRads)*math.Pow(math.Sin(deltaLng/2), 2)
+	a := math.Pow(math.Sin(deltaPhi/2), 2) + math.Cos(phi1)*math.Cos(phi2)*
+		math.Pow(math.Sin(deltaLambda/2), 2)
 	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
 
 	return earthRadiusMeters * c
